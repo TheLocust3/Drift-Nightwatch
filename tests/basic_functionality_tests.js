357ab73f0bdfcd712bcdcd83516158ff539d2a7e
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 19:42:36 -0400

    Condense some basic tests

--- tests/basic_functionality_tests.js
+++ tests/basic_functionality_tests.js
@@ -5,17 +5,11 @@ module.exports = {
     helpers.setupTest(browser)
     browser.end();
   },
-  'Does Chat Open' : function (browser) {
+  'Does Chat Open/Close' : function (browser) {
     helpers.setupTest(browser)
     browser
       .pause(1000)
       .verify.elementPresent('div._1b8HzQM8nmpP_VSMq7OnIz')
-      .end();
-  },
-  'Does Chat Close' : function (browser) {
-    helpers.setupTest(browser)
-    browser
-      .pause(1000)
       .click('button.DISMISS')
       .end();
   },
@@ -24,12 +18,6 @@ module.exports = {
     browser
       .pause(1000)
       .assert.containsText("div._3LgOTzOP49NkcKVg-VuAeA", "Jake Kinsella")
-      .end();
-  },
-  'Check Chat Welcome Message' : function (browser) {
-    helpers.setupTest(browser)
-    browser
-      .pause(1000)
       .assert.containsText("div.dp2Cn7rCPUkibY9nD2WFj", "This is a test")
       .end();
   }
