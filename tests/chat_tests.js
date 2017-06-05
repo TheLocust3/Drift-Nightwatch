6da42590749264135be9cfa93263bdf49fe239ae
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 19:41:15 -0400

    Add email test

--- tests/chat_tests.js
+++ tests/chat_tests.js
@@ -13,5 +13,16 @@ module.exports = {
     .pause(1000)
     .assert.containsText("div.XnctIpXq756HWNS1GLwPT", "test")
     .end();
+  },
+  'Check Email Message' : function (browser) {
+    helpers.setupTest(browser)
+    helpers.enterText('textarea._1eY_aSnr3MDhZqacV8fugZ', 'test', browser)
+    browser
+    .pause(1000)
+    .verify.elementPresent('form._1Gtju_Jekx_uqFMJML29ZR')
+    .verify.elementPresent('button._1pX4AgCirrd3ONwyrdTrtK')
+    .assert.containsText("label._1xnWH0aQJFEhmp915uALhK", "EMAIL ADDRESS")
+    .assert.containsText("button._1pX4AgCirrd3ONwyrdTrtK", "SUBMIT")
+    .end();
   }
 };
