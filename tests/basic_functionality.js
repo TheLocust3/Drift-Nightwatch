c4aa4d3dc3e296554ed305ed4b54734609056646
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 18:51:19 -0400

    Create function for setting up test

--- tests/basic_functionality.js
+++ tests/basic_functionality.js
@@ -1,42 +1,41 @@
+function setupTest(browser) {
+  browser
+    .url(browser.launchUrl)
+    .waitForElementVisible('body', 1000)
+    .waitForElementVisible('iframe#drift-widget', 1000)
+    .frame('drift-widget')
+    .click('button.CHAT')
+}
+
 module.exports = {
   'Does Chat Exist' : function (browser) {
-    browser
-      .url(browser.launchUrl)
-      .waitForElementVisible('body', 1000)
-      .waitForElementVisible('iframe#drift-widget', 1000)
-      .frame('drift-widget')
-      .click('button.CHAT')
-      .end();
+    setupTest(browser)
+    browser.end();
   },
   'Does Chat Open' : function (browser) {
+    setupTest(browser)
     browser
-      .url(browser.launchUrl)
-      .waitForElementVisible('body', 1000)
-      .waitForElementVisible('iframe#drift-widget', 1000)
-      .frame('drift-widget')
-      .click('button.CHAT')
       .pause(1000)
       .verify.elementPresent('div._1b8HzQM8nmpP_VSMq7OnIz')
       .end();
   },
-  'Check Chat Name' : function (browser) {
+  'Does Chat Close' : function (browser) {
+    setupTest(browser)
     browser
-      .url(browser.launchUrl)
-      .waitForElementVisible('body', 1000)
-      .waitForElementVisible('iframe#drift-widget', 1000)
-      .frame('drift-widget')
-      .click('button.CHAT')
+      .pause(1000)
+      .click('button.DISMISS')
+      .end();
+  },
+  'Check Chat Name' : function (browser) {
+    setupTest(browser)
+    browser
       .pause(1000)
       .assert.containsText("div._3LgOTzOP49NkcKVg-VuAeA", "Jake Kinsella")
       .end();
   },
   'Check Chat Welcome Message' : function (browser) {
+    setupTest(browser)
     browser
-      .url(browser.launchUrl)
-      .waitForElementVisible('body', 1000)
-      .waitForElementVisible('iframe#drift-widget', 1000)
-      .frame('drift-widget')
-      .click('button.CHAT')
       .pause(1000)
       .assert.containsText("div.dp2Cn7rCPUkibY9nD2WFj", "This is a test")
       .end();
