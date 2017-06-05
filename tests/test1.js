432014dbf948a6b224ca5139ed75c03a58f19a5b
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 16:38:55 -0400

    Add gitignore, set lauchurl

--- tests/test1.js
+++ tests/test1.js
@@ -1,8 +1,7 @@
 module.exports = {
   'Demo test' : function (browser) {
-    browser
-      .url(browser.launchUrl)
-      // ...
-      .end();
+    browser.url(browser.launchUrl);
+    
+    browser.end();
   }
 };
