063bf15bfa562f3708f8608c92095010b12180d5
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 19:31:24 -0400

    Create test for checking sent message

--- tests/chat_tests.js
+++ tests/chat_tests.js
@@ -5,5 +5,13 @@ module.exports = {
     helpers.setupTest(browser)
     helpers.enterText('textarea._1eY_aSnr3MDhZqacV8fugZ', 'test', browser)
     browser.end();
+  },
+  'Check Sent Message' : function (browser) {
+    helpers.setupTest(browser)
+    helpers.enterText('textarea._1eY_aSnr3MDhZqacV8fugZ', 'test', browser)
+    browser
+    .pause(1000)
+    .assert.containsText("div.XnctIpXq756HWNS1GLwPT", "test")
+    .end();
   }
 };
