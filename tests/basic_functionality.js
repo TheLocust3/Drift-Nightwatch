c664c1d86419dd356a83385b5122e3863f8fbc72
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 17:40:36 -0400

    Create test for make chat open

--- tests/basic_functionality.js
+++ tests/basic_functionality.js
@@ -4,8 +4,19 @@ module.exports = {
       .url(browser.launchUrl)
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('iframe#drift-widget', 1000)
-      .frame('iframe#drift-widget')
-      .click('iframe#drift-widget')
+      .frame('drift-widget')
+      .click('button.CHAT')
+      .end();
+  },
+  'Does Chat Open' : function (browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('iframe#drift-widget', 1000)
+      .frame('drift-widget')
+      .click('button.CHAT')
+      .pause(1000)
+      .verify.elementPresent('div._1b8HzQM8nmpP_VSMq7OnIz')
       .end();
   }
 };
