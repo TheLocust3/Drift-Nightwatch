fd1366890cadec868706d2e2596dca73b837c186
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 17:45:27 -0400

    Add chat name and message tests

--- tests/basic_functionality.js
+++ tests/basic_functionality.js
@@ -18,5 +18,27 @@ module.exports = {
       .pause(1000)
       .verify.elementPresent('div._1b8HzQM8nmpP_VSMq7OnIz')
       .end();
+  },
+  'Check Chat Name' : function (browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('iframe#drift-widget', 1000)
+      .frame('drift-widget')
+      .click('button.CHAT')
+      .pause(1000)
+      .assert.containsText("div._3LgOTzOP49NkcKVg-VuAeA", "Jake Kinsella")
+      .end();
+  },
+  'Check Chat Message' : function (browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('iframe#drift-widget', 1000)
+      .frame('drift-widget')
+      .click('button.CHAT')
+      .pause(1000)
+      .assert.containsText("div.dp2Cn7rCPUkibY9nD2WFj", "This is a test")
+      .end();
   }
 };
