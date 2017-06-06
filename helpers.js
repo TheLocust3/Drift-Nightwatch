d1599b2f457fe694efa0eae3567d336ce51794be
TheLocust3 <jake.kinsella@gmail.com>
2017-06-05 23:00:36 -0400

    Create tests to test which tab is active

--- helpers.js
+++ helpers.js
@@ -1,6 +1,7 @@
 module.exports = {
   setupTest: function (browser) {
     browser
+      .useCss()
       .url(browser.launchUrl)
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('iframe#drift-widget', 1000)
@@ -10,13 +11,16 @@ module.exports = {
 
   enterText: function (selector, text, browser) {
     browser
+    .useCss()
     .setValue(selector, text)
     .pause(1000)
     .setValue(selector, [browser.Keys.ENTER]);
   },
 
   login: function(browser) {
-    browser.url("https://app.drift.com");
+    browser
+    .useCss()
+    .url("https://app.drift.com");
     
     this.enterText('input[type=email]', process.env.USERNAME, browser);
     this.enterText('input[type=password]', process.env.PASSWORD, browser);
